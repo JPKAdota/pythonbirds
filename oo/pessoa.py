f0c49861858098347a090922822d02f424322e7a
João Kadota <joaokadota@gmail.com>
2022-04-28 19:01:15 -0300

    Criado metodos e atributos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__ == '__main__':
     renzo = Pessoa(nome = 'Renzo')
     carlos = Pessoa(renzo, nome = 'Carlos')
@@ -29,4 +37,6 @@ if __name__ == '__main__':
     print(Pessoa.olhos)
     print(carlos.olhos)
     print(renzo.olhos)
+    print(Pessoa.metodo_estatico(), carlos.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), carlos.nome_e_atributos_de_classe())
 
